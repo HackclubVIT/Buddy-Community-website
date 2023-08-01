012ece4a1b5b4546b46e8681d13db8dd473ac742
Aadityaa <aadityaa2606@gmail.com>
2023-08-01 08:04:34 +0530

    added promise function to write data to firebase

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
 const firebaseConfig = {
+    apiKey: "AIzaSyAwk_a5_WqIkeTJwnDMIr0L1nuC8DBni6Y",
+    authDomain: "buddy-community-7d95b.firebaseapp.com",
+    databaseURL: "https://buddy-community-7d95b-default-rtdb.firebaseio.com",
+    projectId: "buddy-community-7d95b",
+    storageBucket: "buddy-community-7d95b.appspot.com",
+    messagingSenderId: "135788365725",
+    appId: "1:135788365725:web:b11542b8e75179e3319174",
+    measurementId: "G-YGET6LFW6E"
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-const contactFormDB = firebase.database().ref('hack-invites');
 
+var contactFormDB = firebase.database().ref('hack-invites');
 
-document.getElementById("contactForm").addEventListener('submit', submitForm)
-
-function submitForm(e) {
+document.getElementById("contactForm").addEventListener('submit', (e) => {
     e.preventDefault();
-    location.replace("/Users/rudreshpandey/Desktop/WebD/hack-invites/success.html");
     const emailid = document.getElementById("emailid").value;
-    const name = document.getElementById("name").value;
+    const name = document.getElementById("name").value;     
     const phone = document.getElementById("phone").value;
     const regno = document.getElementById("regno").value;
+
+    if (!emailid || !name || !phone || !regno) {
+        alert("Please fill in all the fields.");
+        return; // Prevent form submission
+    }
     saveMessages(emailid, name, phone, regno);
-}
+});
+
+
 const saveMessages = (emailid, name, phone, regno) => {
     var newContactForm = contactFormDB.push();
     newContactForm.set({
@@ -25,8 +37,11 @@ const saveMessages = (emailid, name, phone, regno) => {
         phone: phone,
         regno: regno,
     })
-}
+    .then(() => {
+        window.location.replace("/success.html");
+    })
+    .catch((error) => {
+        window.location.replace("/success.html");
+    });
+};
 
-const getElementVal = (id) => {
-    return document.getElementById(id).ariaValueMax;
-}
